c8535e5f1538db97309623f9daed88fef5c8af3c
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-04-27 07:44:42 -0600

    initial commit.

---
+++ Arreglos.s
@@ -0,0 +1,12 @@
+/* -----------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* Sara Maria Perez Echeverria 21371
+* Fabian Estuardo Juarez Tello 21440
+* Organización de computadoras y Assembler
+* Ciclo 1 - 2022
+*
+* Generacion de recibos de compra de los clientes
+* de un supermercado, presentando el registro
+* de productos, cantidad de existencias y el
+* costo unitario.
+ ----------------------------------------------- */
